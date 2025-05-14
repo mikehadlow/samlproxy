9df9a1ca96b334abce367ca3eedf52b3e4828a41
Mike Hadlow <mike@suteki.co.uk>
2025-05-14 12:02:37 +0100

    Export result type from package

--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono"
 import { sayHello } from "common"
+import * as r from "common/result"
 
 const app = new Hono()
-app.get('/', (c) => c.text(sayHello()))
+app.get('/', (c) => c.text(JSON.stringify(r.map(r.from("a"), sayHello))))
 
 export default app
