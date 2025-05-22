0105e57aa5aa6ebb1c45e065dc9f63be7d7916e7
Mike Hadlow <mike@suteki.co.uk>
2025-05-22 11:33:51 +0100

    SP login screen

--- packages/testsp/src/index.ts
+++ packages/testsp/src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import * as fs from "fs"
+import * as path from "path"
 
 const app = new Hono()
 app.use(logger())
 
-app.get('/', (c) => c.text('Hello from the test SP!'))
+app.get("/", (c) => c.redirect("/login"))
+
+app.get("/login", (c) => {
+  const html = fs.readFileSync(path.join(__dirname, "html", "login.html"), "utf8")
+  return c.html(html)
+})
 
 export default app
