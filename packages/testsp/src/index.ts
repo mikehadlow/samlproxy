32235236cf1ef27783b9166231c77b04b7a72878
Mike Hadlow <mike@suteki.co.uk>
2025-05-22 12:04:05 +0100

    add zod

--- packages/testsp/src/index.ts
+++ packages/testsp/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import * as fs from "fs"
 import * as path from "path"
+import * as saml from "common/saml"
 
 const app = new Hono()
 app.use(logger())
@@ -11,6 +12,12 @@ app.get("/", (c) => c.redirect("/login"))
 app.get("/login", (c) => {
   const html = fs.readFileSync(path.join(__dirname, "html", "login.html"), "utf8")
   return c.html(html)
+})
+
+app.post("/login", async (c) => {
+  const body = await c.req.parseBody()
+  console.log(JSON.stringify(body, null, 2))
+  return c.text("Logged in!")
 })
 
 export default app
